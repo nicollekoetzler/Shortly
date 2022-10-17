ba292de1cb4550a7af81791c71c5d19cf2f1322d
nicollekoetzler <nickkoetzler@gmail.com>
2022-10-17 19:08:20 -0300

    set feature to delete url and ranking of most visited urls

--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -51,6 +51,85 @@ export async function getUrlId (req, res) {
     }
   } 
 
-export async function getShortUrl () {
+export async function getShortUrl (req, res) {
 
+    const shortUrl = req.params.shortUrl;
+
+    try{
+        const shortUrlData = await db.query(
+            `SELECT * FROM urls WHERE "shortUrl" = $1`,
+            [shortUrl]
+        )
+        console.log(shortUrlData.rows)
+        
+        if(shortUrlData.rows.length === 0){
+            return res.status(404).send("Url não encontrada")
+        }
+        console.log("oaps")
+        
+        const urlVisitCount = shortUrlData.rows[0].visitCount + 1;
+
+        await db.query(
+            `UPDATE urls SET "visitCount" = $1 WHERE "shortUrl" = $2`, 
+            [urlVisitCount, shortUrl]
+        )
+
+        res.redirect(shortUrlData.rows[0].url);
+
+    }catch(err){
+        console.log(err)
+        res.sendStatus(500);
+    }
 }
+
+export async function deleteUrl (req, res) {
+
+    const id = req.params.id;
+
+    try{
+        const userId = res.locals.userId;
+
+        const urlData = await db.query(
+            `SELECT * FROM urls WHERE "id" = $1`, 
+            [id]
+        )
+
+        if(urlData.rows.length === 0){
+            return res.status(404).send("Url não encontrada");
+        }
+
+        if(urlData.rows[0].userId !== userId){
+            return res.sendStatus(401)
+        }
+
+        await db.query(
+            `DELETE FROM urls WHERE id = $1`,
+            [id]
+        )
+
+        res.sendStatus(204);
+    }catch(err){
+        res.sendStatus(500)
+    }
+}
+
+export async function getRanking(req, res) {
+    try {
+      const usersRanking = await db.query(
+        `SELECT users.id,
+            users.name,
+            COUNT(urls."userId") AS "linksCount",
+            COALESCE(SUM(urls."visitCount"), 0) AS "visitCount"
+        FROM users
+        LEFT JOIN urls ON users.id = urls."userId"
+        GROUP BY users.id
+        ORDER BY "visitCount" DESC
+        LIMIT 10;`
+    );
+      return res.status(200).send(usersRanking.rows);
+  
+    } catch (err) {
+        console.log(err)
+      return res.status(500).send(err);
+    }
+  } 
--- src/routers/urlsRouter.js
+++ src/routers/urlsRouter.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import { validateToken } from "../middlewares/validateToken.js";
 import { validateUrl } from "../middlewares/validateUrl.js";
-import { postUrl, getUrlId, getShortUrl } from "../controllers/urlsController.js";
+import { postUrl, getUrlId, getShortUrl, deleteUrl, getRanking } from "../controllers/urlsController.js";
 
 const router = Router();
 
 router.post("/urls/shorten", validateToken, validateUrl, postUrl);
 router.get("/urls/:id", getUrlId);
 router.get("/urls/open/:shortUrl", getShortUrl);
-// router.delete("/urls/:id", );
+router.delete("/urls/:id", deleteUrl);
+// router.get("/users/me", validateToken, getUsers);
+router.get("/ranking", getRanking);
 
 export default router;
